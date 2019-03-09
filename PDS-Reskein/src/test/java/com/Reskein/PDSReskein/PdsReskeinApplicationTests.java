0316a315ad1258c5725c9a8ac04cf44bc9830612
Pigassi <ppigassi@gmail.com>
2019-03-09 12:21:13 -0300

    Revert "Merge branch 'master' into Desenvolvimento"
    This reverts commit 57f30c0100458cedb1b50b1addd1e4821038309c.

---
+++ PDS-Reskein/src/test/java/com/Reskein/PDSReskein/PdsReskeinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Reskein.PDSReskein;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PdsReskeinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
