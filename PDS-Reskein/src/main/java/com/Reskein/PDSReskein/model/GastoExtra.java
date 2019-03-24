4f9cd32ad07868156f7ebbdb744f392d92324e19
vini2 <vini2@DESKTOP-VATA62C>
2019-03-24 19:02:48 -0300

    Criação das classes Controller, Service and Repository.

--- PDS-Reskein/src/main/java/com/Reskein/PDSReskein/model/GastoExtra.java
+++ PDS-Reskein/src/main/java/com/Reskein/PDSReskein/model/GastoExtra.java
@@ -1,63 +1,63 @@
-//package com.Reskein.PDSReskein.model;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Table;
-//
-//import org.hibernate.validator.constraints.NotBlank;
-//import org.springframework.data.annotation.Id;
-//
-//@Entity
-//@Table(name = "GastoExtra")
-//public class GastoExtra {
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	private int idGastoExtra;
-//	@NotBlank(message = "Descricao obrigatória")
-//	private String descricao;
-//	@NotBlank(message = "Valor obrigatório")
-//	private Double valor;
-//	public int getIdGastoExtra() {
-//		return idGastoExtra;
-//	}
-//	public void setIdGastoExtra(int idGastoExtra) {
-//		this.idGastoExtra = idGastoExtra;
-//	}
-//	public String getDescricao() {
-//		return descricao;
-//	}
-//	public void setDescricao(String descricao) {
-//		this.descricao = descricao;
-//	}
-//	public Double getValor() {
-//		return valor;
-//	}
-//	public void setValor(Double valor) {
-//		this.valor = valor;
-//	}
-//	@Override
-//	public int hashCode() {
-//		final int prime = 31;
-//		int result = 1;
-//		result = prime * result + idGastoExtra;
-//		return result;
-//	}
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (obj == null)
-//			return false;
-//		if (getClass() != obj.getClass())
-//			return false;
-//		GastoExtra other = (GastoExtra) obj;
-//		if (idGastoExtra != other.idGastoExtra)
-//			return false;
-//		return true;
-//	}
-//	
-//	
-//	
-//
-//}
+package com.Reskein.PDSReskein.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "GastoExtra")
+public class GastoExtra {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idGastoExtra;
+	@NotBlank(message = "Descricao obrigatória")
+	private String descricao;
+	@NotBlank(message = "Valor obrigatório")
+	private Double valor;
+	public int getIdGastoExtra() {
+		return idGastoExtra;
+	}
+	public void setIdGastoExtra(int idGastoExtra) {
+		this.idGastoExtra = idGastoExtra;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idGastoExtra;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GastoExtra other = (GastoExtra) obj;
+		if (idGastoExtra != other.idGastoExtra)
+			return false;
+		return true;
+	}
+	
+	
+	
+
+}
--- PDS-Reskein/src/main/java/com/Reskein/PDSReskein/model/Veiculo.java
+++ PDS-Reskein/src/main/java/com/Reskein/PDSReskein/model/Veiculo.java
@@ -1,63 +1,63 @@
-//package com.Reskein.PDSReskein.model;
-//
-//import javax.persistence.Entity;
-//
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Table;
-//
-////import org.hibernate.validator.constraints.NotBlank;
-//import org.springframework.data.annotation.Id;
-//
-//@Entity
-//@Table(name = "Veiculo")
-//public class Veiculo {
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	private int idVeiculo;
-////	@NotBlank(message = "Modelo obrigatório")
-//	private String modelo;
-////	@NotBlank(message = "Lugares obrigatório")
-//	private int lugares;
-//	public int getIdVeiculo() {
-//		return idVeiculo;
-//	}
-//	public void setIdVeiculo(int idVeiculo) {
-//		this.idVeiculo = idVeiculo;
-//	}
-//	public String getModelo() {
-//		return modelo;
-//	}
-//	public void setModelo(String modelo) {
-//		this.modelo = modelo;
-//	}
-//	public int getLugares() {
-//		return lugares;
-//	}
-//	public void setLugares(int lugares) {
-//		this.lugares = lugares;
-//	}
-//	@Override
-//	public int hashCode() {
-//		final int prime = 31;
-//		int result = 1;
-//		result = prime * result + idVeiculo;
-//		return result;
-//	}
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (obj == null)
-//			return false;
-//		if (getClass() != obj.getClass())
-//			return false;
-//		Veiculo other = (Veiculo) obj;
-//		if (idVeiculo != other.idVeiculo)
-//			return false;
-//		return true;
-//	}
-//	
-//	
-//	
-//}
+package com.Reskein.PDSReskein.model;
+
+import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+//import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "Veiculo")
+public class Veiculo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idVeiculo;
+//	@NotBlank(message = "Modelo obrigatório")
+	private String modelo;
+//	@NotBlank(message = "Lugares obrigatório")
+	private int lugares;
+	public int getIdVeiculo() {
+		return idVeiculo;
+	}
+	public void setIdVeiculo(int idVeiculo) {
+		this.idVeiculo = idVeiculo;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getLugares() {
+		return lugares;
+	}
+	public void setLugares(int lugares) {
+		this.lugares = lugares;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idVeiculo;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Veiculo other = (Veiculo) obj;
+		if (idVeiculo != other.idVeiculo)
+			return false;
+		return true;
+	}
+	
+	
+	
+}
